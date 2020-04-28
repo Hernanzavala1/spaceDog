892da96d571a8acf15ef2a3a02c5b48f5029c740
Louis Choicer <louis.choicer@stonybrook.edu>
2020-04-28 19:59:25 -0400

    template for levl

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import TitleScene from './Scenes/TitleScene';
 import OptionsScene from './Scenes/OptionsScene';
 import CreditsScene from './Scenes/CreditsScene';
 import LevelScene from './Scenes/LevelScene';
+import Level2Scene from './Scenes/Level2Scene';
 import Model from './Model';
 
 class Game extends Phaser.Game {
@@ -17,10 +18,12 @@ class Game extends Phaser.Game {
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
     this.scene.add('Title', TitleScene);
+    this.scene.add('Title', Level2Scene);
     this.scene.add('Options', OptionsScene);
     this.scene.add('Credits', CreditsScene);
-    // this.scene.add('Game', GameScene);
+    this.scene.add('Game', GameScene);
     this.scene.add('Level', LevelScene);
+    this.scene.add('Level2', Level2Scene);
     this.scene.start('Boot');
   }
 }
